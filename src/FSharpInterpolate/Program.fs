bfd082c2a71cc12dde3192ad408cca836a5c3168
i.skryabin <i.skryabin@yadro.com>
2024-12-03 19:27:28 +0300

    done?

--- src/FSharpInterpolate/Program.fs
+++ src/FSharpInterpolate/Program.fs
@@ -32,14 +32,14 @@ let windowedMap processors nums =
 
     nums
     |> Seq.scan
-        (fun (prevWindow, prev_out_seq) x ->
+        (fun (prevWindow, _) x ->
             let window =
                 if List.length prevWindow < maxWindowSize then
                     List.append prevWindow [ x ]
                 else
                     List.append (List.tail prevWindow) [ x ]
 
-            let out_seq =
+            let outSeq =
                 processors
                 |> Seq.map (fun (windowSize, proc) ->
                     if windowSize <= List.length window then
@@ -47,7 +47,7 @@ let windowedMap processors nums =
                     else
                         None)
 
-            window, out_seq)
+            window, outSeq)
         (List.empty, Seq.empty)
     |> Seq.map snd
 
@@ -63,27 +63,23 @@ let main (args) =
         printfn "step: %.2f" step
         printfn "methods: %A" methods
 
-        // let points =
-        //     readLines
-        //     |> Seq.map ParseValidation.parseAndValidate
-        //     |> Seq.choose (fun parseRes ->
-        //         match parseRes with
-        //         | Ok point -> Some point
-        //         | Error errList ->
-        //             printfn "Found errors:"
-
-        //             errList
-        //             |> Seq.map ParseValidation.validationErrorToString
-        //             |> Seq.reduce (fun x y -> x + "\n" + y)
-        //             |> printfn "%s"
-
-        //             printfn "Skipping line"
-        //             None)
-        //     |> Seq.cache
         let points =
-            Seq.initInfinite (fun n -> (float) n)
-            |> Seq.map (fun x -> (x, cos x))
-            |> Seq.takeWhile (fun x -> fst x < (float) 100)
+            readLines
+            |> Seq.map ParseValidation.parseAndValidate
+            |> Seq.choose (fun parseRes ->
+                match parseRes with
+                | Ok point -> Some point
+                | Error errList ->
+                    printfn "Found errors:"
+
+                    errList
+                    |> Seq.map ParseValidation.validationErrorToString
+                    |> Seq.reduce (fun x y -> x + "\n" + y)
+                    |> printfn "%s"
+
+                    printfn "Skipping line"
+                    None)
+            |> Seq.cache
 
 
         let processors =
@@ -92,7 +88,8 @@ let main (args) =
 
         points
         |> windowedMap processors
-        |> Seq.map (fun out_seq ->
+        |> Seq.skip 1
+        |> Seq.iter (fun out_seq ->
             out_seq
             |> Seq.zip methods
             |> Seq.iter (fun (m, maybe_points) ->
@@ -102,65 +99,9 @@ let main (args) =
                     out_seq |> Seq.map fst |> Seq.iter (printf "%0.2f\t")
                     printfn ""
                     out_seq |> Seq.map snd |> Seq.iter (printf "%0.2f\t")
-                    printfn ""
-                | None -> ())
+                    printfn "\n"
+                | None -> ()))
 
-            out_seq)
-        |> Seq.skip 1
-        |> Seq.fold
-            (fun acc out_seq ->
-                Seq.zip acc out_seq
-                |> Seq.map (fun (acc_points, maybe_points) ->
-                    match maybe_points with
-                    | Some points -> Seq.append acc_points points |> Seq.sortBy fst |> Seq.distinctBy fst
-                    | None -> acc_points))
-            (Seq.init (Seq.length methods) (fun _ -> Seq.empty))
-        |> Seq.zip methods
-        |> Seq.map (fun (m, points) ->
-            let x, y = points |> Seq.unzip
-
-            Chart.Line(x, y)
-            |> Chart.withTraceName (Name = m.ToString())
-            |> Chart.withLineStyle (Width = 2.0, Dash = StyleParam.DrawingStyle.Solid))
-        |> Seq.append
-            [ let points =
-                  Seq.initInfinite (fun n -> (float) n / 100.0)
-                  |> Seq.map (fun x -> (x, cos x))
-                  |> Seq.takeWhile (fun x -> fst x < 100.0)
-
-              let x, y = points |> Seq.unzip
-
-              Chart.Line(x, y)
-              |> Chart.withTraceName (Name = "Orig")
-              |> Chart.withLineStyle (Width = 2.0, Dash = StyleParam.DrawingStyle.Solid) ]
-        |> Chart.combine
-        |> Chart.show
-        // |> Seq.fold
-        //     (fun acc out_seq ->
-        //         Seq.zip acc out_seq
-        //         |> Seq.map (fun (acc_points, maybe_points) ->
-        //             match maybe_points with
-        //             | Some points -> Seq.append acc_points [ points ]
-        //             | None -> acc_points))
-        //     (Seq.init (Seq.length methods) (fun _ -> Seq.empty))
-        // |> Seq.zip methods
-        // |> Seq.iter (fun (m, points) ->
-        //     printfn "%A" m
-
-        //     points
-        //     |> Seq.iter (fun part ->
-        //         printf "|"
-        //         part |> Seq.map fst |> Seq.iter (printf "%0.2f\t"))
-
-        //     printfn ""
-
-        //     points
-
-        //     |> Seq.iter (fun part ->
-        //         printf "|"
-        //         part |> Seq.map snd |> Seq.iter (printf "%0.2f\t"))
-
-        //     printfn "")
         0
     else
         if parsedStep.IsNone then
